108df09c06936cd12919f53a3d23f6abc921d8a2
Padam Dahal <dahal0514@gmail.com>
2026-07-17 19:52:05 +0000

    minor fixes

--- src/js/app.js
+++ src/js/app.js
@@ -29,53 +29,66 @@ async function getValue(masterUrl, auth, namespace, key){
 }
 
 async function createOrUpdateLocal(namespace,key,value){
-	const localUrl = window.location.origin;
-	const localPathname = window.location.pathname.split("/apps/")[0];
-	const url = localUrl + localPathname + "/dataStore/" +
+
+  const url = "../../../api/dataStore/" +
     encodeURIComponent(namespace) + "/" +
     encodeURIComponent(key);
-	
-	try{
-		await $.ajax({
-		  url:url,
-		  method:"GET"
-		});
-		await $.ajax({
-		  url:url,
-		  method:"PUT",
-		  data: JSON.stringify(value),
-		  contentType:"application/json"
-		});
-		log("UPDATED " + namespace + "/" + key);
-	}catch(e){
-		await $.ajax({
-			url:url,
-			method:"POST",
-			data: JSON.stringify(value),
-			contentType:"application/json"
-		});
-		log("CREATED " + namespace + "/" + key);
-	}
+
+  try{
+    await $.ajax({
+      url:url,
+      method:"GET"
+    });
+
+    await $.ajax({
+      url:url,
+      method:"PUT",
+      data: JSON.stringify(value),
+      contentType:"application/json"
+    });
+
+    log("UPDATED " + namespace + "/" + key);
+
+  }catch(e){
+
+    await $.ajax({
+      url:url,
+      method:"POST",
+      data: JSON.stringify(value),
+      contentType:"application/json"
+    });
+
+    log("CREATED " + namespace + "/" + key);
+  }
 }
 
 async function syncAll(){
+
   $("#log").text("");
+
   const masterUrl = $("#masterUrl").val().replace(/\/$/,'');
   const auth = authHeader(
     $("#username").val(),
     $("#password").val()
   );
+
   const namespaces = await getNamespaces(masterUrl, auth);
+
   for(const namespace of namespaces){
+
     log("Namespace: " + namespace);
+
     const keys = await getKeys(masterUrl, auth, namespace);
+
     for(const key of keys){
+
       const value = await getValue(
         masterUrl,
         auth,
         namespace,
         key
       );
+
       await createOrUpdateLocal(
         namespace,
         key,
@@ -83,10 +96,8 @@ async function syncAll(){
       );
     }
   }
+
   log("DONE");
 }
 
-$(document).ready(function(){
-	$("#syncBtn").on("click", syncAll);
-});
-
+$("#syncBtn").on("click", syncAll);
